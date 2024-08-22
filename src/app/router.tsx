ed3f3d45679230ec874761ef59a031fa8cc8a448
Jona <llamasjonat@gmail.com>
2024-08-22 18:04:19 -0300

    fix(router): fix routers

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -2,24 +2,23 @@ import { useMemo } from 'react';
 import { QueryClient, useQueryClient } from '@tanstack/react-query';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-const ROUTERS = [
-  {
-    path: '/',
-    lazy: async () => {
-      const { HomeRoute } = await import('./routes/home');
-      return { Component: HomeRoute };
+export const createAppRouter = (_queryClient: QueryClient) =>
+  createBrowserRouter([
+    {
+      path: '/',
+      lazy: async () => {
+        const { HomeRoute } = await import('./routes/home');
+        return { Component: HomeRoute };
+      },
     },
-  },
-  {
-    path: '*',
-    lazy: async () => {
-      const { NotFoundRoute } = await import('./routes/not-found');
-      return { Component: NotFoundRoute };
+    {
+      path: '*',
+      lazy: async () => {
+        const { NotFoundRoute } = await import('./routes/not-found');
+        return { Component: NotFoundRoute };
+      },
     },
-  },
-];
-
-export const createAppRouter = (_queryClient: QueryClient) => createBrowserRouter(ROUTERS);
+  ]);
 
 export const AppRouter = () => {
   const queryClient = useQueryClient();
